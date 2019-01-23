2962ef73ef15a4557fb333e945ece5ff90c6c65f
VitorOta <ota.vitor@gmail.com>
2019-01-23 16:49:50 -0200

    [app] configure Timber

---
+++ app/src/main/java/com/vitorota/rickandmorty/RickAndMortyApp.kt
@@ -0,0 +1,25 @@
+package com.vitorota.rickandmorty
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.plant
+
+/**
+ *
+ * @author Vitor Ota
+ * @since 23/01/2019
+ */
+class RickAndMortyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            plant(Timber.DebugTree())
+        }
+    }
+
+}
